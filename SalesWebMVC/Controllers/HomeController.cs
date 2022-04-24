4a79393d2248b11301892d69138afd959242a813
Mateus de Mattos <mateusmattos327@gmail.com>
2022-04-24 14:03:39 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Student"] = "Mateus de Mattos";
+
 
             return View();
         }
